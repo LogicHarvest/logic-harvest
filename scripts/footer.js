17d5cbd259c938c4d8cb2a8e9409ea0bb30754bc
Logic Harvest <152002178+LogicHarvest@users.noreply.github.com>
2024-01-13 11:28:37 +0200

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,2 @@
 const footerElement = document.getElementById('dynamicFooter');
-const copyrightNotice = document.createElement('span');
-copyrightNotice.textContent = '© 2024 Logic Harvest';
-copyrightNotice.style.fontStyle = 'italic';
-copyrightNotice.style.fontSize = 'small';
-footerElement.appendChild(copyrightNotice);
+footerElement.textContent = '© 2024 Logic Harvest';
